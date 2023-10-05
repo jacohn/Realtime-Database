dbdf88babdf745deb56a2246d7b597641a271dae
jacohn <jacohn@gmail.com>
2023-10-04 19:40:59 -0700

    first commit

---
+++ database.js
@@ -0,0 +1,69 @@
+import { initializeApp } from "firebase/app";
+
+(function(){
+
+	// Your web app's Firebase configuration
+	// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+	// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyCIPQ7mYVu5AgsvZKBuSUxvmwwvX0qa0pA",
+	authDomain: "courso-19d5c.firebaseapp.com",
+	projectId: "courso-19d5c",
+	storageBucket: "courso-19d5c.appspot.com",
+	messagingSenderId: "1034126783799",
+	appId: "1:1034126783799:web:00b83ec948e61007e096b2"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig); 
+
+	// handle on firebase db
+	const db = firebase.database();
+
+	// get elements
+	const message = document.getElementById('message');
+	const write   = document.getElementById('write');
+	const read	  = document.getElementById('read');
+	const status  = document.getElementById('status');
+
+	// write
+	write.addEventListener('click', e => {
+		const messages = db.ref('messages');
+
+		// simple id - ok for example, do not use in production
+		const id = (new Date).getTime(); 
+
+		// write to db
+		messages.child(id).set({'message' : message.value})
+			.then(function(){
+				status.innerHTML = "Wrote to DB!";
+			});
+	});
+
+	// read
+	read.addEventListener('click', e => {
+		status.innerHTML = '';
+		const messages = db.ref('messages');
+
+		messages.once('value')
+		  .then(function(dataSnapshot) {
+		  	var data = dataSnapshot.val();
+		  	var keys = Object.keys(data);
+
+		  	keys.forEach(function(key){
+		  		console.log(data[key]);
+				status.innerHTML += JSON.stringify(data[key]) + '<br>';
+		  	});
+		});
+	});
+
+
+}());
